01572721da6d7aa86baf3e36915d9ac66123306e
Alex Junior <alexjvm90@gmail.com>
2021-10-27 22:37:28 -0300

    image, title and fix cursor bug

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,62 @@
+import { imagesArr } from "./imagesArr.js"
+
 const floatingButton = document.querySelector('.floating-button')
+
+const leftArrowDiv = document.querySelector('.left')
+const rightArrowDiv = document.querySelector('.right')
+
+const imageContainer = document.querySelector('.image-container')
 
 const X_DIFF = 10
 const Y_DIFF = 30
+const X_DIFF_SHORTEN = 7
+const Y_DIFF_SHORTEN = 10
 
-const X_DIFF_SHORTEN = 4
-const Y_DIFF_SHORTEN = 7
+let hasToShortenButton = true
 
-const hasToShortenButton = (x) => {
-  const imageContainer = document.querySelector('.image-container')
+const setImages = (pos = 0) => {
+  imageContainer.style.backgroundImage = `url(${imagesArr.at(pos)})`
+}
+
+const rotateImage = (degValue) => {
   const image = document.querySelector('.floating-button-image')
-  const dimensions = imageContainer.getBoundingClientRect()
+  image.style.transform = `rotate(${degValue}deg)`
+}
 
-  if(!(x < dimensions.x || x > dimensions.right)) return true
+leftArrowDiv.addEventListener('mouseenter', () => {
+  floatingButton.classList.remove('floating-button--extra-small')
+  rotateImage(180)
+  hasToShortenButton = false
+})
 
-  if(x < dimensions.x) {
-    image.style.transform = 'rotate(180deg)'
-  } else {
-    image.style.transform = 'rotate(0)'
-  }
+rightArrowDiv.addEventListener('mouseenter', () => {
+  floatingButton.classList.remove('floating-button--extra-small')
+  rotateImage(0)
+  hasToShortenButton = false
+})
 
-  return false
-} 
+imageContainer.addEventListener('mouseover', () => {
+  floatingButton.classList.add('floating-button--extra-small')
+  hasToShortenButton = false
+})
 
 document.addEventListener('mousemove', ({ x, y }) => {
   setTimeout(() => {
     let xPosition = x - X_DIFF
     let yPosition = y - Y_DIFF
 
-    if(hasToShortenButton(x)) {
-      floatingButton.classList.add('floating-button--extra-small')
+    if(hasToShortenButton) {
       xPosition = x - X_DIFF_SHORTEN
       yPosition = y - Y_DIFF_SHORTEN
-    } else {
-      floatingButton.classList.remove('floating-button--extra-small')
     }
   
     floatingButton.style.left = xPosition + 'px'
     floatingButton.style.top = yPosition + 'px'
-  }, 100);
+  }, 120);
 })
+
+const initApp = () => {
+  setImages()
+}
+
+initApp()
---
+++ scripts/imagesArr.js
@@ -0,0 +1,6 @@
+export const imagesArr = [
+  'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8b37bf4f766a_andre-filipe-4ThcC1DtspE-unsplash.jpg',
+  'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8be0694f766b_connor-jolley-y1-jwAqBAoo-unsplash.jpg',
+  'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8bd4874f766d_robin-noguier-1khpK68QqBY-unsplash.jpg',
+  'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8b05134f766c_marc-schadegg-rRvALyIZ8-g-unsplash.jpg'
+]
