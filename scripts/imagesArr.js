3c216331f27656149b509936ecb7823e549fe3c2
Alex Junior <alexjvm90@gmail.com>
2021-11-03 20:53:18 -0300

    fix images

--- scripts/imagesArr.js
+++ scripts/imagesArr.js
@@ -1,18 +1,22 @@
 export const imagesArr = [
   {
-    url: 'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8b37bf4f766a_andre-filipe-4ThcC1DtspE-unsplash.jpg',
+    // url: 'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8b37bf4f766a_andre-filipe-4ThcC1DtspE-unsplash.jpg',
+    url: '../img/iceland.jpg',
     title: 'Iceland'
   },
   {
-    url:   'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8be0694f766b_connor-jolley-y1-jwAqBAoo-unsplash.jpg',
+    // url:   'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8be0694f766b_connor-jolley-y1-jwAqBAoo-unsplash.jpg',
+    url: '../img/canada.jpg',
     title: 'Canada'
   },
   {
-    url: 'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8bd4874f766d_robin-noguier-1khpK68QqBY-unsplash.jpg',
+    // url: 'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8bd4874f766d_robin-noguier-1khpK68QqBY-unsplash.jpg',
+    url: '../img/thailand.jpg',
     title: 'Spain'
   },
   {
-    url: 'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8b05134f766c_marc-schadegg-rRvALyIZ8-g-unsplash.jpg',
+    // url: 'https://uploads-ssl.webflow.com/6178c15da19f8bc0554f7651/6178c15ea19f8b05134f766c_marc-schadegg-rRvALyIZ8-g-unsplash.jpg',
+    url: '../img/spain.jpg',
     title: 'Thailand'
   },
 ]
